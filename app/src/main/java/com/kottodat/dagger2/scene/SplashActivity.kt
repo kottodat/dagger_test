0c1acaeadda53adfbfa18b4c24ebb3e013fbfec9
hana.go <kottodat@naver.com>
2019-04-01 13:14:27 +0900

    project init

---
+++ app/src/main/java/com/kottodat/dagger2/scene/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.kottodat.dagger2.scene
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kottodat.dagger2.R
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
+
